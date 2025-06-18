ba4fc027d2db065c9048fff113da21d4664e5e2e
DELL <mosabaraka@gmail.com>
2025-06-18 14:54:57 +0200

    new

--- app.py
+++ app.py
@@ -165,4 +165,6 @@ def document_details(filename):
     return render_template("details.html", document=doc)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
